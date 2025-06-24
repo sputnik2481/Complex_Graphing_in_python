698e93b1aa5e3a91e11ed6da25caa307d49c04b6
sputnik2481 <veramaek25@gmail.com>
2025-06-24 11:14:06 -0400

    Added in plotting for a fixed real valued part and infinite amounts of complex valued parts

--- 2d_graphing.py
+++ 2d_graphing.py
@@ -2,3 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy import linspace
 
+# graphing for 2D complex infinite lines
+
+x = np.linspace(-100j,100j)
+
+def complex_coordinates_planar(function, graph_color = 'black', point_colors = ['lightseagreen','aquamarine', 'springgreen']):
+    #these two loops just check if theres a color gradient for the line
+    if isinstance(graph_color, list):
+        for i in range(len(graph_color)):
+            plt.plot(function.real[i], function.imag[i], color=graph_color[i])
+    else:
+        plt.plot(function.real, function.imag, color=graph_color)
+
+    for i in range(len(point_colors)):
+        plt.plot(function.real[20+i*5],function.imag[20+i*5], color=point_colors[i], marker = 'o')
+
+    plt.show()
+
+
+complex_coordinates_planar(x+0j)
--- README.txt
+++ README.txt
@@ -2,3 +2,8 @@
 
 This program takes in an input of a function of z such as cos(z) and plots z with respect to z's real parts!
 It can handle anything from basic functions to more complex ones such as sin, cos etc!
+
+2D_GRAPHING.py:
+
+This program generates a graph based on an infinite complex line set at a fixed real value. The function is with respect to x, it cannot handle
+any form of polar coordinates

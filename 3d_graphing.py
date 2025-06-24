28411d245a26f8e093ac5a85b2e3f9cdeebdbfd1
sputnik2481 <veramaek25@gmail.com>
2025-06-24 10:46:57 -0400

    added allowance for a single point to be graphed (as a complex plane) rather than a function

--- 3d_graphing.py
+++ 3d_graphing.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy import linspace
 
+#3d graphing of complex functions!
 
 #initlialization of my XY and Z axes. Y is already complex based on personal preference
 x = linspace(-5,5,100)
@@ -12,7 +13,11 @@ z = X + Y
 #input for this is a function with respect to z. An example would be graph_3D_complex_functions(z**2)
 def graph_3D_complex_functions(function):
     # for readability as typically Z is used
-    Z = function
+    if isinstance(function, int):
+        Z = function + 0*X + 0*Y
+
+    else:
+        Z = function
 
     fig = plt.figure('3D Complex Function', figsize=(5,4.), dpi = 100)
     ax = fig.add_subplot(projection='3d')
@@ -21,8 +26,7 @@ def graph_3D_complex_functions(function):
     ax.plot_surface(X.real, Y.imag, Z.real, rstride=5, cstride=5,
                     cmap='viridis')
 
-    #allows the extreme points to be much more clear then the generic bounds
-    ax.set_zlim3d(np.amin(Z[30]).real, np.amax(Z[70]).real)
+    #allows the extreme points to be much more clear than the generic bounds
+    ax.set_zlim3d(np.amin(Z[30]).real, np.amax(Z[70]).real+0.000000000000001)
 
     plt.show()
-
